e2fe6d51f81fa7ee20beae5dbd3fce226320be41
Layne Harris <helsmack@gmail.com>
2016-05-10 22:06:29 -0400

    demo

--- web/index.php
+++ web/index.php
@@ -2,6 +2,12 @@
 
 require('../vendor/autoload.php');
 
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
